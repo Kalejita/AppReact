5149cb1bf0720a8556b154901974e08fdec29609
Alejandra Moreno <89276369+Kalejita@users.noreply.github.com>
2025-07-25 18:10:21 -0500

    Update ContentRating.jsx

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -1,15 +1,49 @@
-
 import React, { Component } from 'react';
 import './ContentRating.css';
 
 class ContentRating extends Component {
   constructor() {
     super();
+     this.state = {
+      likes: 0,
+      dislikes: 0,
+      totalRatings: 0,
+
+      handleLike:() => {
+        this.setState((prevState) => ({
+          likes: prevState.likes + 1,
+          totalRatings: prevState.totalRatings + 1
+        }));
+      },
+      handleDislike:() => {
+        this.setState((prevState) => ({
+          dislikes: prevState.dislikes + 1,
+          totalRatings: prevState.totalRatings + 1
+        }));
+      }
+
+    };
+    
   }
   render() {
     return (
      <>
-     <h1>Text Content Rating</h1>
+    <div className='content-rating'>
+        <h1>Lorem Ipsum</h1>
+        <p>
+        "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+        "No hay nadie que ame el dolor mismo, que lo busque, lo encuentre y lo quiera, simplemente porque es el dolor."
+        </p>
+        <div className='rating-buttons'>
+            <button className="like-button" onClick={this.state.handleLike}>
+                Me gusta ({this.state.likes})
+            </button>
+            <button className="dislike-button" onClick={this.state.handleDislike}>
+                No me gusta ({this.state.dislikes})
+            </button>
+            <p>Total de interacciones: {this.state.totalRatings}</p>
+        </div>
+    </div>
      </>
     );
   }
